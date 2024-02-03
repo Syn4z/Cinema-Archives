597fd4a796d7b75ae7a94cfc7ef003abf8f70e75
syn4z <iatcosorin797@gmail.com>
2024-02-03 16:30:31 +0200

    refactor: update findLinks; replace incorrect names

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Personal Archives
+# Cinema Archives
 
 ## Usage
 
--- src/findLinks.py
+++ src/findLinks.py
@@ -1,11 +1,10 @@
 from imdb import IMDb
 
-
 def getImdbLinks(inputFile, choice):
     ia = IMDb()
     imdbLinks = {}
 
-    with open(inputFile, "r") as inputData:
+    with open(inputFile+'.txt', "r") as inputData:
         data = inputData.read().splitlines()
 
     for field in data:
@@ -15,13 +14,17 @@ def getImdbLinks(inputFile, choice):
             searchResults = ia.search_movie(field)
 
         if searchResults:
-            data = searchResults[0]
+            imdb_name = searchResults[0]['name'] if choice == "actors" else searchResults[0]['title']
+            if imdb_name.lower() != field.lower():
+                print(f"Warning: The name '{field}' does not match exactly with IMDb. Using IMDb name '{imdb_name}' instead.")
             if choice == "actors":
-                imdbId = data.personID
+                imdbId = searchResults[0].personID
                 imdbLink = f"https://www.imdb.com/name/nm{imdbId}/"
             elif choice == "movies" or choice == "shows":
-                imdbId = data.movieID
+                imdbId = searchResults[0].movieID
                 imdbLink = f"https://www.imdb.com/title/tt{imdbId}/"
-            imdbLinks[field] = imdbLink
+            imdbLinks[imdb_name] = imdbLink
+        else:
+            print(f"No results found for '{field}' in IMDb.")
 
     return imdbLinks
